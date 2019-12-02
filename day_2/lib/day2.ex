a58364909b40715679dc034451a3a89cbcc41550
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2019-12-02 20:23:29 +0100

    Day 2 part 2

--- day_2/lib/day2.ex
+++ day_2/lib/day2.ex
@@ -11,11 +11,64 @@ defmodule Day2 do
   end
 
 
-  def main do
+  @spec part1 :: [integer]
+  def part1 do
     input_to_int_list()
+    |> set_initial_values(12, 2)
     |> process_program
   end
 
+  @spec part2 :: number
+  def part2 do
+    program = input_to_int_list()
+    target = 19690720
+    {noun, verb} = find_target_noun_and_verb(program, target)
+
+    100 * noun + verb
+  end
+
+
+  @doc """
+    Given a program and a target value for position 0, finds noun and verb which
+    would yield that value
+
+    Examples:
+    iex> Day2.find_target_noun_and_verb([1, 1, 0, 0, 99], 2)
+    {1, 1}
+
+    iex> Day2.find_target_noun_and_verb([2, 1, 0, 0, 99], 2)
+    {1, 2}
+
+  """
+  @spec find_target_noun_and_verb([integer], integer) :: {integer, integer}
+  def find_target_noun_and_verb(program, target) do
+    for(noun <- 1..100, verb <- 1..100, do: {noun, verb})
+    |> Enum.find(fn {noun, verb} ->
+      program
+      |> set_initial_values(noun, verb)
+      |> process_program
+      |> Enum.at(0) == target
+    end)
+  end
+
+
+  @doc """
+    Sets the initial value for a program, replacing the second and third argument for the input
+
+    Examples:
+    iex> Day2.set_initial_values([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], 1, 2)
+    [1, 1, 2, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+
+    iex> Day2.set_initial_values([1, 1, 1, 4, 99, 5, 6, 0, 99], 4, 5)
+    [1, 4, 5, 4, 99, 5, 6, 0, 99]
+
+  """
+  @spec set_initial_values([integer], integer, integer) :: [integer]
+  def set_initial_values(program, first, second) do
+    program
+    |> List.replace_at(1, first)
+    |> List.replace_at(2, second)
+  end
 
   @doc """
     Processes a full program, instruction by instruction, returns the final program
